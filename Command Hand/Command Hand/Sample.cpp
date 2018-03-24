f9b7e2a7a9e8984ca656b8839c45c8d653b9aa8d
AZ Vasquez <azvasquez99@gmail.com>
2018-03-23 23:33:26 -0400

    got movecursor to work

--- Command Hand/Command Hand/Sample.cpp
+++ Command Hand/Command Hand/Sample.cpp
@@ -1,6 +1,14 @@
+//for system
 #include <stdlib.h>
 
+//for SetCursorPos(x,y)
+#include <Windows.h>
+
 int main() {
-	system("cmd.exe");
+	//system("cmd.exe");
+	for (int i = 0; i < 1000; i++) {
+		SetCursorPos(i, i);
+		Sleep(1);
+	}
 	return 0;
 }
