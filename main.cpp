51088cd6ccb98750fdde0d2b7d29f1641eb276c2
AZ Vasquez <azvasquez99@gmail.com>
2018-04-20 01:31:07 -0400

    removed old main()

--- main.cpp
+++ main.cpp
@@ -15,39 +15,7 @@
 
 using namespace std;
 using namespace cv;
-/*
-int main()
-{
-	int clk = 0;
 
-	MyVideo mv;
-	VideoCapture vc(0);
-	if (!mv.isOpened()) return -1;
-
-	namedWindow("Video");
-	Mat curr;
-
-	GestureRecognition gr;
-
-	Gesture* currentGesture;
-
-	while (char(waitKey(1)) != 'q' && mv.isOpened())
-	{
-		clk++;
-
-		//for showing the Mat -- eventually we will have it show on the debugging GUI
-		curr = *mv.getFrame();
-		currentGesture = gr.process(&curr);
-		if (curr.empty()) break;
-		Drawer::draw(curr, *currentGesture);
-		imshow("Video", curr);
-
-
-	}
-	return 0;
-	
-}
-*/
 
 int main()
 {
