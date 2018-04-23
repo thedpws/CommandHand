6dc2d56fd1771a1640b870c4d72d38e4e22d21c0
joeylu38 <joeylu@ufl.edu>
2018-04-23 00:19:17 -0400

    Camelcase to underscore and added more spaces

--- CursorControl.cpp
+++ CursorControl.cpp
@@ -1,9 +1,9 @@
 #include "CursorControl.h"
 
+//Given x and y coordinates of two different sized screens, return new coordinates that maps
+//and scales what the display on the first screen is showing onto the second screen
 cv::Point CursorControl::mapPoint(int x1, int y1, int x2, int y2, cv::Point p)
 {
-  //Given x and y coordinates of two different sized screens, return new coordinates that maps
-  //and scales what the display on the first screen is showing onto the second screen
 
     int x = (double)p.x / (double)x1 * (double)x2;
     int y = (double)p.y / (double)y1 * (double)y2;
@@ -16,13 +16,13 @@ void moveCursor(cv::Mat videoFeed, Gesture g)
 {
 	cv::Size matSize = videoFeed.size();
 	cv::Point screenPos = CursorControl::mapPoint(
-		matSize.width, 
+		matSize.width,
 		matSize.height,
-		GetSystemMetrics(SM_CXSCREEN), 
-		GetSystemMetrics(SM_CYSCREEN), 
+		GetSystemMetrics(SM_CXSCREEN),
+		GetSystemMetrics(SM_CYSCREEN),
 		*g.getPoint()
 	);
-	
+
 	SetCursorPos(screenPos.x, screenPos.y);
 }
 */
@@ -37,7 +37,7 @@ void cursorClick()
 	inputs[0].mi.dwflags = MOUSEEVENTF_LEFTDOWN;
 	inputs[1].type = INPUT_MOUSE;
 	inputs[1].mi.dwflags = MOUSEEVENTF_LEFTUP;
-	
+
 	SendInput(2, INPUTS, sizeof(INPUT));
 }
 */
--- Gesture.cpp
+++ Gesture.cpp
@@ -11,21 +11,25 @@ Gesture::Gesture()
 
 void Gesture::update() {}
 
+//Returns the gesture's point
 cv::Point* Gesture::getPoint()
 {
 	return p;
 }
 
+//Sets the gesture's point
 void Gesture::setPoint(cv::Point* p)
 {
 	this->p = p;
 }
 
+//Returns the gesture's ID
 int Gesture::getID()
 {
 	return id;
 }
 
+//Sets the gesture's ID
 bool Gesture::setID(int id)
 {
 	this->id = id;
